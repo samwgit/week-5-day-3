671aba5eeb6801e9c90916213e9de101300bfcf7
Samuel Wilkins <samuel.wilkins.official@gmail.com>
2022-09-22 22:50:34 -0600

    push

---
+++ client/app/env.js
@@ -0,0 +1,6 @@
+export const dev = window.location.origin.includes('localhost')
+export const baseURL = dev ? 'http://localhost:3000' : ''
+export const useSockets = false
+export const domain = 'samw.us.auth0.com'
+export const audience = 'https://dugus-api.com'
+export const clientId = 'TLcu1s3Bekhiygy7aaSTltbowa4fuh34'
